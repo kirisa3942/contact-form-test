66c8b963411495a27400e9f0658b3579a1b8391f
kirisa <kirisa.qulle@gmail.com>
2024-12-19 05:26:16 +0900

    レイアウトの変更

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -14,9 +14,12 @@
 <body>
     <header class="header">
         <div class="header__inner">
-            <a class="header__logo" href="/">FashionablyLate</a>
+            <a class="header__logo" href="/">
+                FashionablyLate
+            </a>
         </div>
     </header>
+    
     <main>
         @yield('content')
     </main>
